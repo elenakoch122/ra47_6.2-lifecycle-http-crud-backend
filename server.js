2edb59e1b7c574f426e65475c97840405b3ded28
Elena Kochegarova <lelet@ngs.ru>
2023-05-01 11:54:56 +0700

    s

--- server.js
+++ server.js
@@ -15,25 +15,25 @@ let nextId = 1;
 const router = new Router();
 
 router.get('/notes', async (ctx, next) => {
-    ctx.response.body = notes;
+  ctx.response.body = notes;
 });
 
 router.post('/notes', async(ctx, next) => {
-    notes.push({...ctx.request.body, id: nextId++});
-    ctx.response.status = 204;
+  notes.push({content: ctx.request.body, id: nextId++});
+  ctx.response.status = 204;
 });
 
 router.delete('/notes/:id', async(ctx, next) => {
-    const noteId = Number(ctx.params.id);
-    const index = notes.findIndex(o => o.id === noteId);
-    if (index !== -1) {
-        notes.splice(index, 1);
-    }
-    ctx.response.status = 204;
+  const noteId = Number(ctx.params.id);
+  const index = notes.findIndex(o => o.id === noteId);
+  if (index !== -1) {
+      notes.splice(index, 1);
+  }
+  ctx.response.status = 204;
 });
 
 app.use(router.routes()).use(router.allowedMethods());
 
-const port = process.env.PORT || 7777;
+const port = process.env.PORT || 7070;
 const server = http.createServer(app.callback());
 server.listen(port, () => console.log('server started'));
